fd614c7069256b92dd7223ab401f1a010ea6f2cc
Taral Ahir <31511632+tahir3@users.noreply.github.com>
2017-11-15 18:04:40 -0500

    Update Character_Select.java

--- character/Character_Select.java
+++ character/Character_Select.java
@@ -1,50 +1,101 @@
 package character;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Character_Select {
-	public static void character_Select(){
-		int response = 0;
-		Scanner input = new Scanner(System.in);
+	static int response = 0;
+	static Scanner input = new Scanner(System.in);
+
+	public static Character_Class character_Select(){
+		
 		/*Here I am asking you which character you would like to be. */
-		System.out.println("Which character class would you like to play as for this session?:"
-				+ "\n\t1.) Warrior"
-				+ "\n\t2.) Orc"
-				+ "\n\t3.) Wizard"
-				+ "\n\t4.) Elf"
-				+ "\n\t5.) Dwarf");
-		response = input.nextInt();
 		while (true) {
+			try{
+				System.out.println("Which character class would you like to play as for this session?:"
+					+ "\n\t1.) Warrior"
+					+ "\n\t2.) Orc"
+					+ "\n\t3.) Wizard"
+					+ "\n\t4.) Elf"
+					+ "\n\t5.) Dwarf");
+			    response = input.nextInt();	
+			    if (response >=1 && response <=5){
+			    	break;
+			    }
+			    else{
+			    	System.out.println("Please enter a number between 1-5");
+			    }
+				}catch(InputMismatchException e){
+					System.out.println("Please enter a valid response");
+					}input.nextLine();
+			}
 			if(response == 1) {
-				System.out.println("You have chosen to be a warrior!");
 				Character_Warrior character = new Character_Warrior();
-				break;
+				displayWarriorChars(character);
+				
+				return character;
 			}
 			else if(response == 2) {
-				System.out.println("You have chosen to be an orc!");
 				Character_Orc character = new Character_Orc();
-				break;
+				System.out.println("You have chosen to be an Orc");
+				return character;
 			}
 			else if(response == 3) {
-				System.out.println("You have chosen to be a wizard!");
 				Character_Wizard character = new Character_Wizard();
-				break;
+				System.out.println("You have chosen to be a Wizard");
+				return character;
 			}
 			else if(response == 4) {
-				System.out.println("You have chosen to be an elf!");
 				Character_Elf character = new Character_Elf();
-				break;
+				System.out.println("You have chosen to be an Elf");
+				return character;
 			}
 			else if(response == 5) {
-				System.out.println("You have chosen to be a dwarf!");
 				Character_Dwarf character = new Character_Dwarf();
-				break;
+				System.out.println("You have chosen to be a Dwarf");
+				return character;
 			}
 			else {
 				System.out.println("Invalid Response. Please enter a number from the list above.");
 				response = input.nextInt();
 				}
+			return null;
 			}
+	
+	       public static void displayWarriorChars (Character_Warrior warrior){			
+		
+		    System.out.println("You have chosen to be a Warrior");
+		    if (warrior != null){	
+		    System.out.println ("\n-----------------------------");
+		    System.out.println ("Charecter-Traits");
+		    System.out.println ("-----------------------------");
+		    System.out.format("%10s%15s%15s%15s%20s", "Speak", "Speed", "Strength", "Health", "Intelligence");
+		    System.out.println ("");		    		   
+		    System.out.format ("%10s%15s%15s%15s%20s", ""+warrior.speak, warrior.speed, warrior.strength, ""+warrior.health, warrior.intelligence);
+		    System.out.println ("");
+		    System.out.println ("\n-----------------------------");
+		    System.out.println ("Defense");
+		    System.out.println ("-----------------------------");
+		    System.out.format("%10s%15s%15s%15s%20s", "Armor", "Boost", "Damage", "Defense", "Resistance");
+		    System.out.println ("");		       
+		    System.out.format ("%10s%15s%15s%15s%20s", ""+warrior.armor, warrior.boost, warrior.damage, ""+warrior.defense, warrior.resistance);
+		    System.out.println ("");	
+		
+		    System.out.println ("\n-----------------------------");
+		    System.out.println ("Weapons");
+		    System.out.println ("-----------------------------");
+		    System.out.format("%10s%15s%15s%15s%20s", "Primary weapon damage", "  Primary weapon", " Weapon damage", " Secondary weapon damage", "Secondary weapon");
+		    System.out.println ("");		       
+		    System.out.format ("%10s%25s%10s%20s%20s", ""+warrior.primary_weapon_damage, warrior.primaryWeapon, warrior.weapon_damage, ""+warrior.secondary_weapon_damage, warrior.secondaryWeapon);
+		    System.out.println ("");
+		    
+		    System.out.println ("\n-----------------------------");
+		    System.out.println ("Miscellaneous");
+		    System.out.println ("-----------------------------");
+		    System.out.format("%10s%15s", "Magic","Money");
+		    System.out.println ("");		       
+		    System.out.format ("%10s%15s", warrior.magic, warrior.money);
+		    System.out.println ("");
+		    }
+	          }
 		}
-
-}
